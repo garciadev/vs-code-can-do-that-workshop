bc64ac396d777ff13f49444bf27a52d3e650ab1d
Burke Holland <burkeholland@gmail.com>
2019-08-02 15:49:29 -0500

    Update exercise 6 for simplicity

--- exercise-6-remote-development/start/routes/index.js
+++ exercise-6-remote-development/start/routes/index.js
@@ -2,14 +2,18 @@ var express = require("express");
 var router = express.Router();
 var axios = require("axios");
 
-const API_BASE = "https://i-enjoy-lamp.azurewebsites.net/api";
+const API_BASE = "https://lifx-lamp-api.azurewebsites.net/api";
 
 router.get("/setColor", async (req, res, next) => {
   const color = req.query.color;
 
   // call the API to set the lamp color
-  await axios.get(`${API_BASE}/setColor?color=${color}`);
-  res.json({ color: color });
+  try {
+    await axios.get(`${API_BASE}/setColor?color=${color}`);
+    res.json({ color: color });
+  } catch (err) {
+    res.status = 500;
+  }
 });
 
 module.exports = router;
